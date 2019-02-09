3482a2af8cf01dfc574bdb138dba41021840e846
jangcoding <coder.jang@gmail.com>
2019-02-08 23:06:20 -0500

    Initial Commit

---
+++ Edge_filter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Edge_filter
+//
+//  Created by 장지선 on 2/8/19.
+//  Copyright © 2019 Geesun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
